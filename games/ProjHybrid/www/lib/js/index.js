d4de9c8d11b201721a062b51c5891a6ba32e3e8d
smartpascal <warleyalex@yahoo.com.br>
2016-08-08 20:41:47 +0200

    add projHybrid1

---
+++ games/ProjHybrid/www/lib/js/index.js
@@ -0,0 +1,70 @@
+var _app = {
+	// Application Constructor
+	initialize : function () {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents : function () {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+	
+	onDeviceReady : function () {
+		// ready
+		app.receivedEvent('deviceready');
+	},
+	// Update DOM on a Received Event
+	receivedEvent : function (id) {
+		var parentElement = document.getElementById(id);
+		var listeningElement = parentElement.querySelector('.listening');
+		var receivedElement = parentElement.querySelector('.received');
+
+		listeningElement.setAttribute('style', 'display:none;');
+		receivedElement.setAttribute('style', 'display:block;');
+
+		console.log('Received Event: ' + id);
+	}
+};
+
+var myApp = new Framework7();
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+		// Because we use fixed-through navbar we can enable dynamic navbar
+		dynamicNavbar : true,
+		domCache : true
+	});
+	
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+    //    myApp.alert('Here comes About page');
+    }
+})
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+  // myApp.alert('Here comes About page');
+})	
+
+_app.initialize();
